a1db026926994217925b71deb70718b8a25802e3
pra-sen-jit <pd2.aiml.tmsl@ticollege.org>
2023-09-29 23:40:15 +0530

    rendering er gondogol debugged + nav bar changes

--- script.js
+++ script.js
@@ -1,62 +1,62 @@
-gsap.to("#nav",{
-    backgroundColor: "#000",
-    duration: 1,
-    paddingTop: "40px",
-    height: "100px",
-    scrollTrigger:{
-        trigger: "#nav",
-        scroll:"body",
-        start:"top -10%",
-        end:"top -7%",
-        scrub: 1
-    }
+gsap.to(".header-top",{
+  backgroundColor: "#000",
+  duration: 1,
+  paddingTop: "40px",
+  height: "100px",
+  scrollTrigger:{
+      trigger: "#nav",
+      scroll:"body",
+      start:"top -10%",
+      end:"top -7%",
+      scrub: 1
+  }
 })
 
-gsap.to(".back",{
-    backgroundColor: "#000",
-    scrollTrigger:{
-        trigger:".back",
-        scroller:"body",
-        start:"top -25%",
-        end: "top -70%",
-        scrub: 2
-    }
+gsap.to("#header",{
+  backgroundColor: "#000",
+  scrollTrigger:{
+      trigger:".back",
+      scroller:"body",
+      start:"top -25%",
+      end: "top -70%",
+      scrub: 2
+  }
 })
 
 gsap.from(".main,.NextInfo",{
-    y:70,
-    opacity:0,
-    duration:1,
-    stagger:0.6,
-    scrollTrigger:{
-        trigger:".main,.NextInfo",
-        scroller:"body",
-        start:"top 60%",
-        end:"top 58%",
-        scrub:2
-    }
+  y:70,
+  opacity:0,
+  duration:1,
+  stagger:0.6,
+  scrollTrigger:{
+      trigger:".main,.NextInfo",
+      scroller:"body",
+      start:"top 60%",
+      end:"top 58%",
+      scrub:2
+  }
 })
 
 gsap.from("#colon1",{
-    y:-70,
-    x:-70,
-    scrollTrigger:{
-        trigger:"#colon1",
-        scroller:"body",
-        start:"top 55%",
-        end:"top 45%",
-        scrub:4
-    }
+  y:-70,
+  x:-70,
+  scrollTrigger:{
+      trigger:"#colon1",
+      scroller:"body",
+      start:"top 55%",
+      end:"top 45%",
+      scrub:4
+  }
 })
 
 gsap.from("#colon2",{
-    y:70,
-    x:70,
-    scrollTrigger:{
-        trigger:"#colon1",
-        scroller:"body",
-        start:"top 55%",
-        end:"top 45%",
-        scrub:4
-    }
+  y:70,
+  x:70,
+  scrollTrigger:{
+      trigger:"#colon1",
+      scroller:"body",
+      start:"top 55%",
+      end:"top 45%",
+      scrub:4
+  }
 })
